6bd29eec0ed74bf7bb4980fc58e747893b81912e
MoizBabinwale <mbabinwale@gmail.com>
2023-03-11 00:23:05 +0530

    First client commit

---
+++ src/Pages/Post/Post.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import './Post.css';
+
+const MakePost = () => {
+  const [image, setImage] = useState(null);
+  const [title, setTitle] = useState('');
+  const [tags, setTags] = useState('');
+  const navigate = useNavigate();
+
+  const User = useSelector((state) => state.currentUserReducer);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Create a FormData object to store the form data
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('tags', tags);
+    formData.append('image', image);
+
+    // Make a POST request to the server to save the post
+    try {
+      const response = await fetch('http://localhost:5000/posts/upload', {
+        method: 'POST',
+        crossDomain: true,
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          base64: image,
+          title,
+          tags
+        })
+      })
+      const data = await response.json();
+      console.log(data);
+      navigate('/DisplayPost');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const imageToBase64 = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      console.log(reader.result);
+      setImage(reader.result);
+    };
+
+    reader.onerror = (error) => {
+      console.error('Error: ', error);
+    };
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <div>
+        <label>
+          Image:
+          <input type="file" accept="image/*" onChange={imageToBase64} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Title:
+          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Tags:
+          <input type="text" value={tags} onChange={(e) => setTags(e.target.value)} />
+        </label>
+      </div>
+      <button type="submit">Submit</button>
+      <img src={image} height={150} width={150} />
+    </form>
+  );
+};
+
+export default MakePost;
---
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './LeftSidebar.css'
+import { NavLink } from 'react-router-dom'
+import Globe from '../../assets/Globe.svg'
+
+const LeftSidebar = () => {
+  const onClickMenu = () => {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  return (
+    <div className='left-sidebar'>
+      <button id='menu' onClick={onClickMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </button>
+      <nav className='side-nav' id='myDIV'>
+        <NavLink to='/' className='side-nav-links' activeclassname='active'>
+          <p>Home</p>
+        </NavLink>
+        <div className='side-nav-div'>
+          <div><p>PUBLIC</p></div>
+          <NavLink to='/Questions' className='side-nav-links' activeclassname='active'>
+            <img src={Globe} alt="Globe" />
+            <p style={{ paddingLeft: "10px" }}> Questions </p>
+          </NavLink>
+          <NavLink to='/Tags' className='side-nav-links' activeclassname='active' style={{ paddingLeft: "40px" }}>
+            <p>Tags</p>
+          </NavLink>
+          <NavLink to='/Users' className='side-nav-links' activeclassname='active' style={{ paddingLeft: "40px" }}>
+            <p>Users</p>
+          </NavLink>
+          <NavLink to='/DisplayPost' className='side-nav-links' activeclassname='active' style={{ paddingLeft: "40px" }}>
+            <p>Posts</p>
+          </NavLink>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default LeftSidebar
